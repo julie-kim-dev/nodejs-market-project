9ca298fb8ee57a3afb57e9f36b376a8395345787
julie kim <julie.kim.dev@gmail.com>
2024-01-23 14:26:27 +0900

    schema 설계

--- schemas/products.schema.js
+++ schemas/products.schema.js
@@ -1,22 +1,31 @@
 import mongoose from 'mongoose';
 
-const productSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
+const productSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    userName: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: Number,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ['FOR_SALE', 'SOLD_OUT'],
+      default: 'FOR_SALE',
+    },
   },
-  content: {
-    type: String,
-    required: true,
-  },
-  userName: {
-    type: String,
-    required: true,
-  },
-  password: {
-    type: Number,
-  },
-});
+  { timestamps: true }
+);
 
 // productSchema를 바탕으로 Product모델을 생성하여, 외부로 내보냄
 export default mongoose.model('Product', productSchema);
