0d9473ec1aea5ba735140e3ea7c8eca2665fbcd2
julie kim <julie.kim.dev@gmail.com>
2024-01-23 14:35:41 +0900

    app.js 파일에 router 셋팅

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 require('dotenv').config();
 
 import express from 'express';
-import connect from './schemas/index.js';
+import connect from './schemas/index.js'; //MongoDB에 접속해야 하기 때문에 가지고 옴
 import productRouter from './routes/products.router.js';
 
 const app = express();
 const PORT = 3000;
 
-connect(); // schema index.js 연결
+connect(); // schema index.js 연결 -> 실제로 MongoDB에 접속
 
-// Express에서 req.body에 접근하여 body 데이터를 사용할 수 있도록 설정합니다.
+// Express에서 req.body에 접근하여 body 데이터를 사용할 수 있도록 설정합니다. -> 미들웨어 파싱
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
